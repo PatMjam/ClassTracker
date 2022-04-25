84bc9efcd2f5da9504cbb105e713457061ef182f
PatMjam <41456487+PatMjam@users.noreply.github.com>
2022-04-25 20:51:40 +1000

    Added changes and Unit Controllers and a DTO for  handling the creation of new TafeClasses

---
+++ ClassTracker/Controllers/TafeClassController.cs
@@ -0,0 +1,70 @@
+﻿using ClassTracker.DTO;
+using ClassTracker.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClassTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TafeClassController : ControllerBase
+    {
+        // GET: api/<TafeClassController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TafeClassController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TafeClassController>
+        //Example DTO usage in place of the full entity
+        [HttpPost]
+        public ActionResult<TafeClass> Post(TafeClassCreate tafeClass)
+        {
+            if (tafeClass == null)
+            {
+                return BadRequest();
+            }
+            //convert the DTO to an Entity
+            TafeClass createdTafeClass = new TafeClass();
+            {
+                Name = tafeClass.Name,
+                Description = tafeClass.Description,
+                Location = tafeClass.Location,
+                StartTime = tafeClass.StartTime,
+                DurationMinutes = tafeClass.DurationMinutes,
+                TeacherId = tafeClass.TeacherId
+            };
+
+            save the entity
+            _context.TafeClasses.Add(createdTafeClass);
+            _context.SaveChanges();
+
+            return CreatedAtAction("Post", createdTafeClass);
+        }
+
+        // PUT api/<TafeClassController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TafeClassController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ClassTracker/DTO/TafeClassCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassTracker.DTO
+{
+    public class TafeClassCreate
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime? StartTime { get; set; }
+        public int? DurationMinutes { get; set; }
+        public int TeacherId { get; set; }
+
+    }
+}
